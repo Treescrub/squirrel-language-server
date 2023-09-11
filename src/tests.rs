f0aa755df02db11fac787b4cf49d5693ca304dc5
Treescrub <treescrub@gmail.com>
2023-09-10 19:42:13 -0600

    Initial test module

---
+++ src/tests.rs
@@ -0,0 +1,20 @@
+use crate::lexer::{Lexer, TokenType};
+
+#[test]
+fn hex_literal() {
+    let mut lexer = Lexer::new("0x12345678");
+    lexer.lex();
+
+    assert_eq!(lexer.tokens.len(), 1);
+    assert_eq!(lexer.tokens[0].token_type, TokenType::IntegerLiteral);
+    assert_eq!(lexer.tokens[0].nvalue, Some(305419896));
+}
+
+#[test]
+fn hex_literal_overflow() {
+    let mut lexer = Lexer::new("0x123456789");
+    lexer.lex();
+
+    assert_eq!(lexer.tokens.len(), 1);
+    assert_eq!(lexer.tokens[0].token_type, TokenType::Invalid);
+}
